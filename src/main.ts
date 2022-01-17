6989d1828dd94b8e51af1599715a77f03a1c9775
angrylid <zm4399@gmail.com>
2022-01-17 20:02:01 +0800

    add naive ui

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { create, NButton } from "naive-ui"
+// 等宽字体
+import "vfonts/FiraCode.css"
+// 通用字体
+import "vfonts/Lato.css"
+import { createApp } from "vue"
+import App from "./App.vue"
 
-createApp(App).mount('#app')
+const naive = create({
+    components: [NButton],
+})
+const app = createApp(App)
+app.use(naive)
+app.mount("#app")
